1df34ee655d67cdfcb254668a26614983f5fe103
Chirila Gabriela <gabrielachirila239@gmail.com>
2023-10-24 23:20:53 +0300

    add ex2&3 in lab4

--- Lab04/lab4.py
+++ Lab04/lab4.py
@@ -8,6 +8,7 @@
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
+import math
 
 lambda1 = 20 # parametrul distributiei Poisson
 
@@ -16,7 +17,7 @@ deviatie_comanda = 0.5
 
 alpha = 1 # media distributiei exponentiale a pregatirii comenzii in minute
  
-num_samples = 10000  
+num_samples = 1000
 
 num_clienti = stats.poisson.rvs(lambda1, size=num_samples)
 
@@ -26,14 +27,32 @@ timp_pregatire = stats.expon.rvs(scale=1/alpha, size=num_samples)
 
 timp_total = timp_comanda + timp_pregatire
 
-timp_total_all_clients = timp_total.sum()
 
 print("Media clientilor care intra in restaurant:", num_clienti.mean())
-print("Media timpul total de servire pentru un client (minute):", timp_total.mean())
-print("Timpul total de servire pentru clienti (minute):", timp_total_all_clients)
+print("Media timpului total de servire pentru un client (minute):", timp_total.mean())
+
+# 2.Determinaţi care este (cu aproximaţie) α maxim pentru a le putea servi mancarea intr-un timp mai scurt
+# de 15 minute tuturor clienţilor care intră într-o oră, cu o probabilitate de 95%. (1p - deadline: sfârşitul zilei de
+# seminar)
+
+probabilitate = 0.95
+t_maxim = 15
+
+alpha_max = -t_maxim / math.log(1 - probabilitate)
+
+print("Valoarea maxima a lui alpha:", alpha_max)
+
+# 3.Cu α astfel calculat, determinaţi timpul mediu de aşteptare pentru a fi servit al unui client.
+
+alpha_max = 1 / alpha_max 
+
+medie_comanda = medie_comanda + 1 / alpha_max
+
+print("Timpul mediu de așteptare pentru a fi servit unui client este:", medie_comanda, "minute")
 
 plt.hist(timp_total, bins=40, density=True, label='Timp servire')
 plt.xlabel('Timp (minute)')
 plt.ylabel('Probabilitate')
 plt.legend()
 plt.show()
+
